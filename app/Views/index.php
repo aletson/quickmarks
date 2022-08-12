19fff0ff30fc7160ec793ff482e937fd683b7c90
aletson <me@ajl.io>
2022-08-12 13:18:16 -0400

    sdf

--- app/Views/index.php
+++ app/Views/index.php
@@ -24,7 +24,7 @@
               foreach ($thisInstance->marks as $thisMark) {
       ?>
                 <tr data-mark="<?= $thisMark->id; ?>" data-instance-id="<?= $thisMark->instance; ?>">
-                  <td><?php echo $thisMark->name; ?> <?php if ($thisMark->instance > 1) echo '(I' . $thisMark->instance . ')'; ?><br /><small><i>&emsp;<?php if (isset($thisMark->nickname)) echo $thisMark->nickname; ?></i></small></td>
+                  <td><?php echo $thisMark->name; ?> <?php if ($thisMark->instance > 1) echo '(I' . $thisMark->instance . ')'; ?><?php if (isset($thisMark->nickname)) echo '<br /><small><i>&emsp;' . $thisMark->nickname . '</i></small>'; ?></td>
                   <td>last reported <span class="time" data-killed="<?= isset($thisMark->last_kill) ? $thisMark->last_kill : 'never'; ?>"></span><?php if (!isset($thisMark->last_kill) || $thisMark->last_kill < time() - 14400) { ?>
                       <!-- create button if >4 hours --><button class="btn btn-outline-light markButton btn-sm" data-mark="<?= $thisMark->id; ?>" data-instance="<?= $thisMark->instance; ?>">&nbsp;mark dead&nbsp;</button><?php } ?>
           <?php }
