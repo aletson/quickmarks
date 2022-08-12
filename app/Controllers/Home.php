5419d9570c065c69f923626c822b087b157bcaa0
aletson <me@ajl.io>
2022-08-12 11:44:28 -0400

    move to marks2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,31 +9,36 @@ use App\Models\Reports;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        $this->Marks = new Marks();
-        $this->Expansions = new Expansions();
-        $this->Zones = new Zones();
-        $expansions = $this->Expansions->orderBy('id', 'desc')->get()->getResult();
-        foreach ($expansions as $thisExpansion) {
-          $thisExpansion->instances = [];
-          for ($instanceId = 1; $instanceId <= $thisExpansion->instance_count; $instanceId++) {
-            $thisExpansion->instances[$instanceId] = new \stdClass();
-            $thisExpansion->instances[$instanceId]->marks = $this->Marks->join('zones z', 'marks.zone_id = z.id')->join('reports r', 'r.mark_id = marks.id')->where('z.expansion_id', $thisExpansion->id)->where('r.instance_id', $instanceId)->select('marks.*, z.name as zone_name, max(r.tod) as tod')->get()->getResult();
-          }
+  public function index()
+  {
+    $this->Marks = new Marks();
+    $this->Expansions = new Expansions();
+    $this->Zones = new Zones();
+    $this->Reports = new Reports();
+    $expansions = $this->Expansions->orderBy('id', 'desc')->get()->getResult();
+    foreach ($expansions as $thisExpansion) {
+      $thisExpansion->zones = $this->Zones->where('expansion_id', $thisExpansion->id)->orderBy('id', 'desc')->get()->getResult();
+      foreach ($thisExpansion->zones as $thisZone) {
+        for ($instanceId = 1; $instanceId <= $thisZone->instances; $instanceId++) {
+          $thisExpansion->instances[$instanceId] = new \stdClass();
+          $thisExpansion->instances[$instanceId]->marks = $this->Marks->join('zones z', 'marks.zone_id = z.id')->join('expansions e', 'z.expansion_id = e.id')->join('reports r', 'r.mark_id = marks.id')->where('r.instance_id', $instanceId)->select('marks.*, z.name as zone_name, e.name as expansion_name, max(r.tod) as tod')->get()->getResult();
         }
-        $data['json_marks'] = json_encode($expansions);
-        //now manipulate the data to pull out by instances
-        echo view('header', $data);
-        echo view('index', $data);
+      }
     }
-    
-    public function killed() {
-      $mark_id = $this->request->getVar('id');
-      $time = intval($this->request->getVar('time'));
-      $this->Marks = new Marks();
-      $data = [ 'last_kill' => $time ];
-      $this->Marks->update($mark_id, $data);
-      echo 'success';
-    }
+    $data['json_marks'] = json_encode($expansions);
+    //now manipulate the data to pull out by instances
+    echo view('header', $data);
+    echo view('index', $data);
+  }
+
+  public function killed()
+  {
+    $mark_id = $this->request->getVar('id');
+    $instance = $this->request->getVar('instance');
+    $time = intval($this->request->getVar('time'));
+    $this->Marks = new Marks();
+    $data = ['last_kill' => $time, 'instance_id' => $instance];
+    $this->Marks->update($mark_id, $data);
+    echo 'success';
+  }
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,4 +1,102 @@
 <!-- CONTENT -->
 
 <section>
-<?php print_r($json_marks); ?>
+
+  <h1>About this page</h1>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Mark name</th>
+        <th>timer</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $expansion = '';
+      foreach ($marks as $thisMark) {
+        if ($thisMark->expansion_name != $expansion_name) {
+          $expansion_name = $thisMark->expansion_name;
+          echo '<tr><td colspan="2"><b><center>' . $expansion_name . '</center></b></td></tr>';
+        } ?>
+        <tr data-mark="<?= $thisMark->id; ?>" data-instance-id="<?= $thisMark->instance_id; ?>">
+          <td><?php echo $thisMark->name; ?> <?php if ($thisMark->instance_id > 1) echo '(I' . $thisMark->instance_id . ')'; ?><br /><small><i>\t<?php if (isset($thisMark->nickname)) echo $thisMark->nickname; ?></i></small></td>
+          <td>last reported <span class="time" data-killed="<?= isset($thisMark->last_kill) ? $thisMark->last_kill : 'never'; ?>"></span><?php if (!isset($thisMark->last_kill) || $thisMark->last_kill < time() - 14400) { ?>
+              <!-- create button if >4 hours --><button class="rounded-full border markButton text-sm" data-mark="<?= $thisMark->id; ?>" data-instance="<?= $thisMark->instance_id; ?>">&nbsp;mark dead&nbsp;</button><?php } ?>
+          <?php } ?>
+    </tbody>
+    <table>
+
+</section>
+
+<!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
+
+<footer>
+
+  <div class="copyrights">
+
+    <p>&copy; <?= date('Y') ?> alli.</p>
+
+  </div>
+
+</footer>
+
+<!-- SCRIPTS -->
+
+<script>
+  function regenCountdowns() {
+    var newDate = new Date;
+    newDate = Math.floor(newDate.getTime() / 1000); // seconds
+    $('.time').each(function() {
+      if ($(this).data('killed') != 'never') {
+        var difference = newDate - $(this).data('killed');
+        var hoursAgo = Math.floor(difference / 3600); //seconds / 60 = minutes, minutes / 60 = hours
+        var color = 'red';
+        if (hoursAgo >= 4 && hoursAgo < 6) {
+          color = 'darkgoldenrod';
+        } else if (hoursAgo >= 6) {
+          color = 'green';
+        }
+        var minutesAgo = Math.floor(difference / 60 % 60);
+        if (minutesAgo < 10) {
+          minutesAgo = '0' + minutesAgo;
+        }
+        $(this).text(hoursAgo + ':' + minutesAgo + ' ago');
+        $(this).css('color', color);
+      }
+    });
+  };
+
+  $(document).ready(function() {
+    $('.time').each(function() {
+      if ($(this).data('killed') == 'never') {
+        $(this).append('never');
+      }
+    });
+  }).on('click', '.markButton', function() {
+    var thisButton = $(this);
+    var mark = $(this).data('mark');
+    var instance = $(this).data('instance');
+    $.post('/killed', {
+        "id": mark,
+        "instance": instance,
+        "time": Date.now() / 1000
+      }) // This can be used to do TOD reporting too, just set the post to the filled value rather than Date.now.
+      .done(function(data) {
+        thisButton.parent().find('.time').each(function() {
+          $(this).data('killed', parseInt(Date.now() / 1000));
+        });
+        thisButton.hide();
+      }).fail(function(data) {
+
+      });
+  });
+  setInterval(regenCountdowns, 1000);
+
+  // check every second if we're above 14400 seconds after and creat ebuttons if necessary
+  // also timers counters or something eventually
+</script>
+
+
+</body>
+
+</html>
