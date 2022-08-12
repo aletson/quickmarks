7cebdfb47514d0d8ceb80aab103e4e189f400774
aletson <me@ajl.io>
2022-08-12 12:14:27 -0400

    asfd

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -25,8 +25,9 @@ class Home extends BaseController
             ->join('zones z', 'marks.zone_id = z.id')
             ->join('reports r', 'r.mark_id = marks.id and r.instance_id = ' . $instanceId, 'left outer')
             ->where('z.id', $thisZone->id)
-            ->where('id is not null')
             ->select('marks.*, max(r.tod) as last_kill')->get()->getResult();
+
+          print_r($this->db->last_query());
           /*if ($thisZone->instances[$instanceId]->marks) {
             foreach ($thisZone->instances[$instanceId]->marks as $thisMark) {
               $thisMark->instance = $instanceId;
