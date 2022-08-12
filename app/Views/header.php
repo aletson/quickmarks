650d9cbeb2fe1f4433ce1e254a07fc7d814afd83
aletson <me@ajl.io>
2022-08-12 13:09:57 -0400

    asdf

--- app/Views/header.php
+++ app/Views/header.php
@@ -6,7 +6,8 @@
 	<meta name="description" content="uwu">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-	<script src="https://cdn.tailwindcss.com"></script>
+	<!--<script src="https://cdn.tailwindcss.com"></script>-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="/js/jquery-3.5.0.js"></script>
 
 	<!-- STYLES -->
